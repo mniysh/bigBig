1bf60d6b2e373e89c59cc0f28f3aee2992ff3b26
wangkai <123940232@qq.com>
2015-09-17 09:33:46 +0800

    增加Fragment基类标题

--- ebang/src/main/java/com/ms/ebangw/fragment/BaseFragment.java
+++ ebang/src/main/java/com/ms/ebangw/fragment/BaseFragment.java
@@ -5,7 +5,11 @@ import android.app.DialogFragment;
 import android.app.Fragment;
 import android.content.Context;
 import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
 
+import com.ms.ebangw.R;
 import com.ms.ebangw.dialog.LoadingDialog;
 import com.ms.ebangw.utils.L;
 import com.umeng.analytics.MobclickAgent;
@@ -30,6 +34,59 @@ public abstract class BaseFragment extends Fragment {
      * 初始化数据
      */
     abstract public void initData();
+
+    /**
+     * @param leftClickLister    返回箭的点击监听
+     * @param left               左边标题
+     * @param title              中间标题
+     * @param right              右边标题
+     * @param rightClickListener 右边标题的点击监听
+     */
+    public void initTitle(View.OnClickListener leftClickLister, String left, String title, String
+        right, View.OnClickListener rightClickListener) {
+        Window window = mActivity.getWindow();
+
+        View backView = window.findViewById(R.id.iv_back);
+        TextView leftTv = (TextView)  window.findViewById(R.id.tv_left);
+        TextView titleTv = (TextView)  window.findViewById(R.id.tv_center);
+        TextView rightTv = (TextView)  window.findViewById(R.id.tv_right);
+        //设置返回箭头
+        if (null != leftClickLister && backView != null) {
+            backView.setOnClickListener(leftClickLister);
+            backView.setVisibility(View.VISIBLE);
+        } else {
+            backView.setVisibility(View.GONE);
+        }
+        //设置左标题
+        if (leftTv != null && !TextUtils.isEmpty(left)) {
+            leftTv.setText(left);
+            leftTv.setVisibility(View.VISIBLE);
+        }
+
+        //设置中间的标题
+        if (titleTv != null && !TextUtils.isEmpty(title)) {
+            titleTv.setText(title);
+            titleTv.setVisibility(View.VISIBLE);
+        }
+
+        //设置右边文字
+        if (rightTv != null && !TextUtils.isEmpty(right)) {
+            rightTv.setText(right);
+            if (null != rightClickListener) {
+                rightTv.setOnClickListener(rightClickListener);
+            }
+            rightTv.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void initTitle(String title) {
+        initTitle(null, null, title, null, null);
+    }
+
+    public void initTitle(String title, String right, View.OnClickListener rightClickListener) {
+        initTitle(null, null, title, right, rightClickListener);
+    }
+
 
     /**
      * 显示进度对话框
