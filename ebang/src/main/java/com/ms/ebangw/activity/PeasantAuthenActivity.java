0459a6af93b78bde267cb50be753f90e02fcb504
杨少华 <15321214698@163.com>
2015-09-18 10:07:16 +0800

    添加务工人认证的第一个页面

--- ebang/src/main/java/com/ms/ebangw/activity/PeasantAuthenActivity.java
+++ ebang/src/main/java/com/ms/ebangw/activity/PeasantAuthenActivity.java
@@ -1,9 +1,11 @@
 package com.ms.ebangw.activity;
 
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
 
@@ -11,43 +13,48 @@ import com.ms.ebangw.R;
 import com.ms.ebangw.fragment.PeasantBankFeagment;
 import com.ms.ebangw.fragment.PeasantBaseFragment;
 
+import butterknife.Bind;
+import butterknife.OnClick;
+
 /**
- * 农民工认证的基本信息页面
+ * 务工人认证的基本信息页面
  */
 public class PeasantAuthenActivity extends BaseActivity implements OnClickListener, OnCheckedChangeListener {
-	private FragmentManager fm;
-	private PeasantBaseFragment pBase;
-	private PeasantBankFeagment pBank;
-	private RadioGroup rGroup;
+
+	@Bind(R.id.bt_next)
+	Button bNext;
+	@OnClick(R.id.bt_next)
+	public void next(){
+
+	}
+	//private RadioGroup rGroup;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.act_peasant_authen);
 		initView();
+		initData();
 		initViewOper();
 		
 	}
 
 	private void initViewOper() {
 		// TODO Auto-generated method stub
-		fm.beginTransaction().replace(R.id.act_pea_frame, pBase).commit();
-		rGroup.setOnCheckedChangeListener(this);
+		//rGroup.setOnCheckedChangeListener(this);
 		initTitle(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				PeasantAuthenActivity.this.finish();
 			}
-		},"返回","农民工认证",null,null);
+		},"返回","务工认证",null,null);
 		
 	}
 
 	public void initView() {
 		// TODO Auto-generated method stub
-		fm=getFragmentManager();
-		pBase=new PeasantBaseFragment();
-		pBank=new PeasantBankFeagment();
-		rGroup=(RadioGroup) findViewById(R.id.act_pea_rg);
+		//rGroup=(RadioGroup) findViewById(R.id.act_pea_rg);
+
 		
 		
 	}
@@ -67,12 +74,10 @@ public class PeasantAuthenActivity extends BaseActivity implements OnClickListen
 	public void onCheckedChanged(RadioGroup group, int checkedId) {
 		// TODO Auto-generated method stub
 		switch (checkedId) {
-		case R.id.act_pea_rb_base:
-			fm.beginTransaction().replace(R.id.act_pea_frame, pBase).commit();
-			break;
-		case R.id.act_pea_rb_bank:
-			fm.beginTransaction().replace(R.id.act_pea_frame, pBank).commit();
-			break;
+//		case R.id.act_pea_rb_base:
+//			break;
+//		case R.id.act_pea_rb_bank:
+//			break;
 
 		default:
 			break;
