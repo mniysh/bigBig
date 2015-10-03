763694501ca4c7e4541efffb0621b32bf7320f38
杨少华 <15321214698@163.com>
2015-10-03 14:41:18 +0800

    调细节

--- ebang/src/main/java/com/ms/ebangw/utils/VerifyUtils.java
+++ ebang/src/main/java/com/ms/ebangw/utils/VerifyUtils.java
@@ -1,6 +1,10 @@
 package com.ms.ebangw.utils;
 
+import android.text.Editable;
+import android.text.Selection;
 import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -95,5 +99,105 @@ public class VerifyUtils {
             return  false;
         }
         return true;
+    }
+    /**
+     * 处理银行卡号
+     * @param view
+     */
+    public static void setBankCard(final EditText view){
+        view.addTextChangedListener(new TextWatcher() {
+            int beforeTextLength = 0;
+            int onTextLength = 0;
+            boolean isChanged = false;
+
+            int location = 0;// 记录光标的位置
+            private char[] tempChar;
+            private StringBuffer buffer = new StringBuffer();
+            int konggeNumberB = 0;
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                beforeTextLength = s.length();
+                if (buffer.length() > 0) {
+                    buffer.delete(0, buffer.length());
+                }
+                konggeNumberB = 0;
+                for (int i = 0; i < s.length(); i++) {
+                    if (s.charAt(i) == ' ') {
+                        konggeNumberB++;
+                    }
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                onTextLength = s.length();
+                buffer.append(s.toString());
+                if (onTextLength == beforeTextLength || onTextLength <= 3
+                        || isChanged) {
+                    isChanged = false;
+                    return;
+                }
+                isChanged = true;
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (isChanged) {
+                    location = view.getSelectionEnd();
+                    int index = 0;
+                    while (index < buffer.length()) {
+                        if (buffer.charAt(index) == ' ') {
+                            buffer.deleteCharAt(index);
+                        } else {
+                            index++;
+                        }
+                    }
+
+                    index = 0;
+                    int konggeNumberC = 0;
+                    while (index < buffer.length()) {
+                        if ((index == 4 || index == 9 || index == 14 || index == 19)) {
+                            buffer.insert(index, ' ');
+                            konggeNumberC++;
+                        }
+                        index++;
+                    }
+
+                    if (konggeNumberC > konggeNumberB) {
+                        location += (konggeNumberC - konggeNumberB);
+                    }
+
+                    tempChar = new char[buffer.length()];
+                    buffer.getChars(0, buffer.length(), tempChar, 0);
+                    String str = buffer.toString();
+                    if (location > str.length()) {
+                        location = str.length();
+                    } else if (location < 0) {
+                        location = 0;
+                    }
+
+                    view.setText(str);
+                    Editable etable = view.getText();
+                    Selection.setSelection(etable, location);
+                    isChanged = false;
+                }
+            }
+        });
+
+
+    }
+
+    /**
+     * 转化卡号格式
+     * @param s
+     * @return
+     */
+    public static String bankCard(String s){
+
+
+        return s.replaceAll(" ","");
     }
 }  
