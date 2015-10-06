009da9b8611d53bb08586e939803083ac364d7ab
wangkai <123940232@qq.com>
2015-10-06 15:46:58 +0800

    修改图片选择取消崩溃的bug

--- ebang/src/main/java/com/ms/ebangw/userAuthen/developers/DevelopersBankVerifyFragment.java
+++ ebang/src/main/java/com/ms/ebangw/userAuthen/developers/DevelopersBankVerifyFragment.java
@@ -199,10 +199,11 @@ public class DevelopersBankVerifyFragment extends BaseFragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode != mActivity.RESULT_OK){
+            return;
+        }
         if (requestCode == REQUEST_CAMERA ) { //拍照返回
-            if (resultCode == mActivity.RESULT_OK) {
-                handleBigCameraPhoto();
-            }
+            handleBigCameraPhoto();
 
         }else if (requestCode == REQUEST_PICK) {
             Uri uri = data.getData();
