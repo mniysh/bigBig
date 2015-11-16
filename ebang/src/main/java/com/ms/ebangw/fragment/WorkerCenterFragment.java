346a9e2511c164bf8d1d35bd15b9a9c28dd354d2
杨少华 <15321214698@163.com>
2015-11-16 09:57:38 +0800

    工人工人中心添加个人详细标题

--- ebang/src/main/java/com/ms/ebangw/fragment/WorkerCenterFragment.java
+++ ebang/src/main/java/com/ms/ebangw/fragment/WorkerCenterFragment.java
@@ -2,25 +2,35 @@ package com.ms.ebangw.fragment;
 
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.ms.ebangw.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * 务工人中心
  * @author wangkai
  *
  */
-public class WorkerCenterFragment extends Fragment {
+public class WorkerCenterFragment extends BaseFragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
     private String mParam1;
     private String mParam2;
+    private ViewGroup contentLayout;
+    private FragmentManager fm;
+
+
+
 
 
 
@@ -44,13 +54,30 @@ public class WorkerCenterFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        fm = getFragmentManager();
+
+    }
+
+    @Override
+    public void initView() {
+        fm.beginTransaction().replace(R.id.fl_head_info, HeadInfoFragment.newInstance("", ""));
+
+    }
+
+    @Override
+    public void initData() {
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_worker_center, container, false);
+        contentLayout = (ViewGroup) inflater.inflate(R.layout.fragment_worker_center, null);
+        ButterKnife.bind(this, contentLayout);
+        initView();
+        initData();
+        return contentLayout;
     }
 
 
